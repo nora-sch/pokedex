921d6b41c3fe4c1ca41bbaefca004215dd0a2a04
nora-sch <nora.schumann.development@gmail.com>
2023-05-10 10:41:43 +0200

    use props to display pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,7 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard(props) {
+  console.log(props);
+  const pokemon = props.pokemon;
   return (
     <figure>
       {pokemon.imgSrc ? (
@@ -18,7 +9,6 @@ function PokemonCard() {
       ) : (
         <p>???</p>
       )}
-
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
