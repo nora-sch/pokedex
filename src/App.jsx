510c248afb9b4687cb4b6f411903c169d3f1791c
nora-sch <nora.schumann.development@gmail.com>
2023-05-10 11:59:57 +0200

    use of state and state modification

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,43 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickPrevious = () => {
+    if(pokemonIndex>0)
+    setPokemonIndex(pokemonIndex - 1);
+ 
+  };
+  const handleClickNext = () => {
+    pokemonIndex<pokemonList.length-1? setPokemonIndex(pokemonIndex + 1):setPokemonIndex(pokemonIndex);
+  };
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]} />
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+       {pokemonIndex>0 ? <button className="previous" onClick={handleClickPrevious}>
+          Previous
+        </button> : ''}
+        {pokemonIndex<pokemonList.length-1 ? <button className="next" onClick={handleClickNext}>
+          Next
+        </button>:''}
       </div>
     </>
   );
